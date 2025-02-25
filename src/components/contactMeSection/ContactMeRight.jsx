4bb98f778f54092531d21980c669363a061add56
Vinay Krishna <vinaydiamod02@gmail.com>
2025-02-25 18:18:46 +0530

    Fixed image paths and updated alt text

--- src/components/contactMeSection/ContactMeRight.jsx
+++ src/components/contactMeSection/ContactMeRight.jsx
@@ -4,11 +4,11 @@ import ContactSocial from "./ContactSocial";
 const ContactMeRight = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-12">
-      <img
-        src="../../public/images/email-image.png"
-        alt="email image"
-        className="max-w-[300px]"
-      />
+       <img
+          src="/vk-website/images/email-image.png"
+          alt="Vinay Krishna"
+          className="max-w-[300px]"
+        />
       <ContactInfo />
       <ContactSocial />
     </div>
--- src/components/heroSection/HeroImage.jsx
+++ src/components/heroSection/HeroImage.jsx
@@ -4,11 +4,11 @@ const HeroImage = () => {
   return (
     <div className="relative self-end h-full w-full items-center justify-center">
       <div className=" h-full w-full">
-        <img
-          src="/vk-website/images/HexaPic.png"
-          alt="Vinay krishna"
-          className="w-auto h-auto md:max-w-[570px] sm:max-w-[380px] absolute bottom-[0px] z-10 left-[50%] -translate-x-[50%]"
-        />
+      <img
+        src="/vk-website/images/HexaPic.png"
+        alt="Vinay Krishna"
+        className="w-auto h-auto md:max-w-[570px] sm:max-w-[380px] absolute bottom-[0px] z-10 left-[50%] -translate-x-[50%]"
+       />
         <div className="w-full h-full absolute  bottom-[-20%] -z-10 flex justify-center items-center rotate-90">
           <PiHexagonThin className=" md:h-[90%] sm:h-[120%] min-h-[600px] w-auto text-orange opacity-70  animate-[spin_20s_linear_infinite] " />
         </div>
