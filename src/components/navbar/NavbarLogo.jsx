9903bd24b878b98a25705f103b51c3c886591e18
Vinay Krishna <vinaydiamod02@gmail.com>
2025-02-25 18:44:17 +0530

    Fixed image filename to match case sensitivity

--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -5,7 +5,7 @@ const NavbarLogo = () => {
         Vinay Krishna
       </h1>
       <h1 className="text-white font-special font-extrabold text-4xl md:hidden sm:block">
-        AQ
+        VK
       </h1>
     </div>
   );
